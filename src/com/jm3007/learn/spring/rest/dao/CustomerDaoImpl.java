71d4d6727f109aa2d29a88b129f92133763dc369
Code WellnWill <code.wnw@gmail.com>
2019-02-10 13:35:22 +0530

    Spring REST

---
+++ src/com/jm3007/learn/spring/rest/dao/CustomerDaoImpl.java
@@ -0,0 +1,51 @@
+package com.jm3007.learn.spring.rest.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jm3007.learn.spring.rest.model.Customer;
+
+@Repository
+public class CustomerDaoImpl implements CustomerDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	@Transactional
+	public Long saveCustomer(Customer customer) {
+		return (Long) sessionFactory.openSession().save(customer);
+	}
+
+	@Override
+	@Transactional
+	public Customer getCustomer(Long id) {
+		return sessionFactory.openSession().get(Customer.class, id);
+	}
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return sessionFactory.openSession().createQuery("from Customer").getResultList();
+	}
+
+	@Override
+	@Transactional
+	public void updateCustomer(Customer customer) {
+		sessionFactory.getCurrentSession().saveOrUpdate(customer);
+	}
+
+	@Override
+	@Transactional
+	public void deleteCustomer(Long id) {
+		Session session = sessionFactory.getCurrentSession();
+		Customer customer = session.get(Customer.class, id);
+		session.delete(customer);
+	}
+
+}
---
+++ src/com/jm3007/learn/spring/rest/model/Customer.java
@@ -0,0 +1,59 @@
+package com.jm3007.learn.spring.rest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "JM3007_CUSTOMER")
+public class Customer {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "age")
+	private int age;
+
+	public Customer(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public Customer() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
